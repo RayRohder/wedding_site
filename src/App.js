db02744e706f89d206d4f0823b2f19ef20fb91d2
Raymond Rohder <RayRohder@Outlook.com>
2018-08-09 14:37:51 +0200

    base site

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Components
+import Header from './Components/header';
+import Footer from './Components/footer';
+import HomePage from './Components/Pages/homePage';
+import Nav from './Components/nav';
+
+// includes
+import './Assets/css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          
-          Hello world!
-        </p>
+        <Header />
+        <Nav />
+        <HomePage />
+
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Pages/homePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Morbi augue sem, vestibulum non turpis scelerisque, ultricies vehicula mi.
+        Donec fermentum arcu non augue tristique pretium.
+        Nulla vestibulum elementum odio, eu semper est commodo id.
+        Morbi mattis gravida est ut faucibus.
+        Quisque suscipit lorem quis nunc gravida, mollis lobortis eros volutpat.
+        Vestibulum sodales metus posuere, auctor velit vel, imperdiet enim.
+        Suspendisse bibendum volutpat ex, eu porttitor lectus convallis et.
+        Sed venenatis orci egestas posuere tincidunt.
+        Praesent laoreet ullamcorper efficitur.
+      </div>
+    );
+  }
+}
+
+export default HomePage;
